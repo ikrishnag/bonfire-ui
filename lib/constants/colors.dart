c014972823a7be4f1aaa70186eac5494ca47a3c0
ikrishnag <e.krishnagupta@gmail.com>
2025-01-17 23:15:57 +0530

    feat: add lightPurple and badgePurple colors to AppColors for enhanced theming options

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,6 +4,10 @@ class AppColors {
   static const Color purple = Color.fromRGBO(139, 136, 239, 1);
   static const Color headingPurple = Color.fromRGBO(204, 200, 255, 1);
   static const Color darkGrey = Color.fromRGBO(35, 42, 46, 1);
+  static const Color lightPurple = Color.fromRGBO(203, 201, 255, 0.7);
+  static const Color badgePurple = Color.fromRGBO(181, 178, 255, 1);
+
+
 
 
 }
